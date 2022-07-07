ed5578ea2becb2dfed9c2a1253180d364f7a9c30
Kouta Nakano <knakano95@gmail.com>
2022-07-07 20:57:23 +0900

    setup webpack

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-require('dotenv').config();
 import { Auth, Preset, Function, Pattern } from './commands';
 
 // Auth.getQR('1', 'geniuskouta').then(res => console.log(res));
 // Preset.execute('1', Preset.Model.PULSE, 100).then(res => console.log(res));
-Function.execute('1', Function.Model.STOP, 1).then(res => console.log(res));
+// Function.execute('1', Function.Model.STOP, 1).then(res => console.log(res));
 
 // Pattern.execute('1', {
 //   version: 2,
