982582aca712eda96addce23b3fc521041108f80
Kouta Nakano <knakano95@gmail.com>
2022-07-07 21:04:17 +0900

    expose methods to window

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-import { Auth, Preset, Function, Pattern } from './commands';
-
-// Auth.getQR('1', 'geniuskouta').then(res => console.log(res));
-// Preset.execute('1', Preset.Model.PULSE, 100).then(res => console.log(res));
-// Function.execute('1', Function.Model.STOP, 1).then(res => console.log(res));
-
-// Pattern.execute('1', {
-//   version: 2,
-//   interval: 1000,
-//   instructions: [Pattern.Model.VIBRATE, Pattern.Model.ROTATE]
-// }, 100, [20, 5, 10, 2, 10, 15, 30]).then(res => console.log(res));
+export { Auth, Preset, Function, Pattern } from './commands';
